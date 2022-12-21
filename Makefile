ccf82649262d7bfe9b7bf356caa38744354cfbad
halvarez <hansalvarezherencia@protonmail.com>
2022-12-21 18:06:21 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,69 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/06/22 16:55:50 by halvarez          #+#    #+#              #
+#    Updated: 2022/12/21 18:01:53 by halvarez         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#Default build version = release
+build		:=
+
+#Compilation of the mandatory part
+NAME    	= fdf
+SRCS_DIR	= ./src/
+SRCS		= 00_main.c
+OBJS		= $(addprefix ${SRCS_DIR}, ${SRCS:.c=.o})
+DEPS		= $(addprefix ${SRCS_DIR}, ${SRCS:.c=.d})
+
+#Libraries
+OMLX		= -I/usr/include -Imlx_linux
+CLIB		= -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lm -lXext -lX11
+CLIB		+= -lz -Llibft -lft
+
+#Conditionnal flags depending on the building version
+cflags.rls		:= -Wall -Wextra -Werror -MMD -MP
+cflags.gdb		:= -g3
+cflags.san		:= -g3 -fsanitize=address
+CFLAGS			= ${cflags.rls} ${cflags.${BUILD}}
+
+#Rules to other makefiles
+CC			= cc
+MAKE		= make
+CLEAN		= make clean
+FCLEAN		= make fclean
+RM			= rm -rf
+
+.c.o:	
+		${CC} ${CFLAGS} ${OMLX} -c $< -o $@
+
+#Rules to compile
+${NAME}:${OBJS}
+		${MAKE} -C mlx_linux 
+		${MAKE} -C libft
+		${CC} ${CFLAGS} ${OBJS} ${CLIB} -o ${NAME}
+
+all:    ${NAME}
+
+bonus:	all
+
+#Cleaning rules
+clean:
+		${RM} ${OBJS} ${DEPS}
+		${MAKE} clean -C mlx_linux
+		${MAKE} clean -C libft
+
+fclean: clean
+		${RM} ${NAME}
+		${MAKE} clean -C libft
+
+re:     fclean all
+
+#Dependencies list
+-include ${DEPS}
+
+.PHONY: all clean fclean re bonus
---
+++ src/12_bresenham.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   12_bresenham.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 13:49:38 by halvarez          #+#    #+#             */
+/*   Updated: 2022/09/21 18:54:04 by halvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "t_fdf.h"
+#include "ft_fdf.h"
+
+/*
+void	ft_bresenham(t_data *data, t_point pt1, t_point pt2)
+{
+	int	slope;
+	int	x;
+	int	x_max;
+	int	y;
+	int	y0;
+
+	slope = (pt2.y3d - pt1.y3d) / (pt2.x3d - pt1.x3d);
+	if (pt1.x3d < pt2.x3d)
+	{
+		x = pt1.x3d;
+		x_max = pt2.x3d;
+		y_max = pt2.y3d;
+	}
+	else
+	{
+		x = pt2.x3d;
+		x_max = pt1.x3d;
+		y_max = pt1.y3d;
+	}
+	while (x < x_max)
+	{
+		y = slope * x + y0;
+		img_pixel_put(&data->img, x, y, data);
+		x++;
+	}
+}
+*/
+
+void	ft_bresenham(t_data *data, t_point pt1, t_point pt2)
+{
+	int	dx;
+	int	ex;
+	int	dy;
+	int	ey;
+
+	ex = abs(pt2.x3d - pt1.x3d);
+	ey = abs(pt2.y3d - pt1.y3d);
+	dx = 2 * ex;
+	dy = 2 * ey;
+	if (dx > dy)
+		vert_bresenham(data, dx, dy, ex);
+	else
+		horz_bresenham(data, dx, dy, ex);
+}
+
+void	vert_bresenham(t_data *data, int dx, int dy, int ex)
+{
+	int	i;
+
+	i = 0;
+	while (i < dx)
+	{
+		img_pixel_put(&data->img, data->pt1.x3d, data->pt1.y3d, data);
+		i++;
+		if (data->pt1.x3d > data->pt2.x3d)
+			data->pt1.x3d -= 1;
+		else
+			data->pt1.x3d += 1;
+		ex -= dy;
+		if (ex < 0)
+		{
+			if (data->pt1.y3d > data->pt2.y3d)
+				data->pt1.y3d -= 1;
+			else
+				data->pt1.y3d += 1;
+			ex += dx;
+		}
+	}
+}
+
+void	horz_bresenham(t_data *data, int dx, int dy, int ey)
+{
+	int	i;
+
+	i = 0;
+	while (i < dy)
+	{
+		img_pixel_put(&data->img, data->pt1.x3d, data->pt1.y3d, data);
+		i++;
+		if (data->pt1.y3d > data->pt2.y3d)
+			data->pt1.y3d -= 1;
+		else
+			data->pt1.y3d += 1;
+		ey -= dx;
+		if (ey < 0)
+		{
+			if (data->pt1.x3d > data->pt2.x3d)
+				data->pt1.x3d -= 1;
+			else
+				data->pt1.x3d += 1;
+			ey += dy;
+		}
+	}
+}
+
+t_matrix	*add_origin(t_matrix *pts, t_matrix *origin)
+{
+	int	j;
+
+	j = 0;
+	if (pts && origin && pts->pxl && origin->pxl)
+	{
+		while (j < pts->col)
+		{
+			pts->pxl[x][j] += origin->pxl[x][0];
+			pts->pxl[y][j] += origin->pxl[y][0];
+			pts->pxl[z][j] += origin->pxl[z][0];
+			j++;
+		}
+	}
+	return (pts);
+}
+
+void	print_commands(void)
+{
+	ft_printf("\nList of commands :\n");
+	ft_printf("Rotate around z axis (phi)\t:\t4 & 6 NumPad\n");
+	ft_printf("Rotate fom z axis (teta)\t:\t5 & 8 NumPad\n");
+	ft_printf("Combined rotation (phi+teta)\t:\t7 & 9 NumPad\n");
+	ft_printf("Modify z value\t\t\t:\t- & + NumPad\n");
+	ft_printf("Translations\t\t\t:\tthe 4 arrows\n");
+	ft_printf("Parallel projection\t\t:\tp\n");
+	ft_printf("Z symmetry\t\t\t:\ts\n");
+	ft_printf("Switch on/off bresemham algo\t:\tb\n");
+	ft_printf("change colors\t\t\t:\tPageUp & PageDown\n");
+	ft_printf("Reset origin\t\t\t:\t0 NumPad\n\n");
+}
