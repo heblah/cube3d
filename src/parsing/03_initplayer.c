2502999fe199c7962ba1053c9c93142c3ae295f4
halvarez <hansalvarezherencia@protonmail.com>
2023-01-11 13:55:30 +0100

    inversion est / west corrigee

--- src/parsing/03_initplayer.c
+++ src/parsing/03_initplayer.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:13:51 by halvarez          #+#    #+#             */
-/*   Updated: 2023/01/11 13:48:25 by halvarez         ###   ########.fr       */
+/*   Updated: 2023/01/11 13:54:39 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,12 @@ static void	get_playerdata(t_player *player,int i, int j, int dir)
 	}
 	else if (dir == 'E')
 	{
-		player->dir.x = -1;
+		player->dir.x = 1;
 		player->dir.y = 0;
 	}
 	else if (dir == 'W')
 	{
-		player->dir.x = 1;
+		player->dir.x = -1;
 		player->dir.y = 0;
 	}
 }
@@ -54,12 +54,12 @@ static void	get_fov(t_data *data, int dir)
 	else if (dir == 'E')
 	{
 		data->plane.x = 0;
-		data->plane.y = 0.66;
+		data->plane.y = -0.66;
 	}
 	else if (dir == 'W')
 	{
 		data->plane.x = 0;
-		data->plane.y = -0.66;
+		data->plane.y = 0.66;
 	}
 }
 
